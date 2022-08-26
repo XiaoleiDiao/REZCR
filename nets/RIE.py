e12e661b5b3e49ca3d708e6f4f9f725f1463b72c
Xiaolei <xiaolei.diao1@gmail.com>
2022-08-26 17:20:40 +0200

    Update RIE.py

--- nets/RIE.py
+++ nets/RIE.py
@@ -1,3 +1,7 @@
+# -------------------------------------#
+#    The model of RIE
+# -------------------------------------#
+
 from collections import OrderedDict
 import math
 import torch
@@ -39,12 +43,10 @@ class RAB(nn.Module):
         super(RAB, self).__init__()
         self.inplanes = [32,64,128,256,512]
         self.planes = [3, 32, 64,128,256,512,1024,1024]
-        # print(self.planes[1:4])
 
         self.conv1 = nn.Conv2d(3, self.inplanes[0], kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(self.inplanes[0])
         self.relu1 = nn.LeakyReLU(0.1)
-
 
         self.layer1 = self._make_layer(self.planes[1:3], layers[0])
         self.layer2 = self._make_layer(self.planes[2:4], layers[1])
@@ -53,10 +55,9 @@ class RAB(nn.Module):
         self.layer5 = self._make_layer(self.planes[5:7], layers[4])
         self.DSAL = DSAL_layer(kernel_size)
 
-
         self.layers_out_filters = [32, 64, 128, 256, 512, 1024]
 
-        # 进行权值初始化
+        # Initialize weights
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
@@ -66,17 +67,15 @@ class RAB(nn.Module):
                 m.bias.data.zero_()
 
     #---------------------------------------------------------------------#
-    #   在每一个layer里面，首先利用一个步长为2的3x3卷积进行下采样
-    #   然后进行残差结构的堆叠
+    #   define layer
     #---------------------------------------------------------------------#
     def _make_layer(self, planes, blocks):
         layers = []
-        # 下采样，步长为2，卷积核大小为3
-        layers.append(("ds_conv", nn.Conv2d(planes[0], planes[1], kernel_size=3,
-                                stride=2, padding=1, bias=False)))
+        # Downsampling, with stride 2 and kernel size 3
+        layers.append(("ds_conv", nn.Conv2d(planes[0], planes[1], kernel_size=3, stride=2, padding=1, bias=False)))
         layers.append(("ds_bn", nn.BatchNorm2d(planes[1])))
         layers.append(("ds_relu", nn.LeakyReLU(0.1)))
-        # 加入残差结构
+        # Add residual structure
         for i in range(0, blocks):
             layers.append(("CBR_{}".format(i), CBR(planes[1], planes)))
         return nn.Sequential(OrderedDict(layers))
@@ -126,8 +125,7 @@ def make_radical_layers(filters_list, in_filters, out_filter):
         conv2d(filters_list[0], filters_list[1], 3),
         conv2d(filters_list[1], filters_list[0], 1),
         conv2d(filters_list[0], filters_list[1], 3),
-        nn.Conv2d(filters_list[1], out_filter, kernel_size=1,
-                                        stride=1, padding=0, bias=True)
+        nn.Conv2d(filters_list[1], out_filter, kernel_size=1, stride=1, padding=0, bias=True)
     ])
     return m
 
@@ -174,21 +172,15 @@ class RIEBody(nn.Module):
 
     def forward(self, x):
         def _branch(last_layer, layer_in):
-            # print("last_layer", last_layer)
-            # print("layer_in", layer_in.shape)
 
             for i, e in enumerate(last_layer):
-                # print("i",i)
-                # print("e",e)
                 layer_in = e(layer_in)
-                # print("layer_in_i", layer_in.shape)
                 if i == 2:
                     out_branch = layer_in
             return layer_in, out_branch
 
         x0 = self.backbone(x) # 13,13,1024
 
-        # 13,13,1024 -> 13,13,512 -> 13,13,1024 -> 13,13,512 -> 13,13,1024 -> 13,13,512
         out0, out0_branch = _branch(self.OPr, x0)
         out11, out11_branch = _branch(self.OPs1, x0)
         out11 = out11.reshape(out11.shape[0], -1)
@@ -204,10 +196,4 @@ if __name__ == '__main__':
     out0, out1=model(input)
     print(out0.shape, out1.shape)
 
-    # model = CBR(inplanes=64, planes=[32,64])
-    # model = RAB([1,2])
-    # model = RAB([1, 2, 8, 8, 4])
-    # out=model(input).cuda
-    # print(out.shape)
-    
-    
+
